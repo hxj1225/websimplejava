313c04e1e7154c218f1871bbad5e220768251357
hxj1225 <hxj1225@163.com>
2013-11-21 15:10:56 +0800

    a

--- src/main/java/com/hxj/websimplejava/concurrent/common/Job.java
+++ src/main/java/com/hxj/websimplejava/concurrent/common/Job.java
@@ -5,33 +5,33 @@ import java.util.concurrent.Callable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+public abstract class Job<T> implements Callable<T> {
 
-public abstract class Job<T> implements Callable<T>{
-	private Logger logger = LoggerFactory.getLogger(Job.class);
+    private Logger logger = LoggerFactory.getLogger(Job.class);
 
-	private String name;
-	
-	public Job(String name) {
-		this.name = name;
-	}
-	
-	public abstract T execute();
+    private String name;
 
-	@Override
-	public T call() throws Exception {
-		logger.info(name + "job execute start");
-		long start = System.currentTimeMillis();
-		T t = this.execute();
-		long end = System.currentTimeMillis();
-		logger.info(name + "job execute end.time:" + (end - start));
-		return t;
-	}
+    public Job(String name){
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public abstract T execute();
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    @Override
+    public T call() throws Exception {
+        logger.info(name + "job execute start");
+        long start = System.currentTimeMillis();
+        T t = this.execute();
+        long end = System.currentTimeMillis();
+        logger.info(name + "job execute end.time:" + (end - start));
+        return t;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
